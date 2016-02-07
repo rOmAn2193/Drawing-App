ef2a22909700cf446780a0f06929cee9d78ceb55
rOmAn2193 <roman_puzey@aol.com>
2016-02-07 11:19:58 -0700

    Initial Commit

---
+++ Draw App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Draw App
+//
+//  Created by Roman on 2/7/16.
+//  Copyright © 2016 Roman Puzey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
